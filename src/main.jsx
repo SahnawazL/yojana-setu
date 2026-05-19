b8f53c24d610f319821dfa67135afd02b0a9ec64
SahnawazL <Balveerdj@gmail.com>
2026-05-19 15:45:46 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import YojanaSetu from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <YojanaSetu />
+  </StrictMode>
+)
